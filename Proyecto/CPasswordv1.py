aa02c4f77dbda30dc224063b29017f4a7ae3781f
GusRojas <32475691+GusRojas@users.noreply.github.com>
2023-06-27 19:57:17 -0600

    Ver2 Final

--- Proyecto/CPasswordv1.py
+++ Proyecto/CPasswordv1.py
@@ -6,8 +6,13 @@ PORT = 65432  # número de puerto
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
+print("Por favor ingresa los requisitos para generar una contraseña")
+print("El siguiente es un ejemplo de como ingresar los requisitos:")
+print("  8,True,False,True,False ")
 requisitos = input('Ingrese los requisitos de la contraseña (longitud,mayusculas,minusculas,numeros,caracteres especiales): ')
 s.sendall(requisitos.encode())
+
+print("Su contraseña: ")
 
 with open('contrasenas.txt', 'w') as f:
     while True:
